e4806b61c8efe26bee65c96d72906220084abccc
scottnuma <scott.numamoto@gmail.com>
2018-04-07 19:31:03 -0700

    Create text based display

--- graph/urls.py
+++ graph/urls.py
@@ -5,8 +5,11 @@ from . import views
 app_name = 'graph'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('department/', views.DepartmentsView.as_view(), name='departments'),
     path('department/<int:pk>/', views.DepartmentView.as_view(), name='department'),
+    path('course/', views.CoursesView.as_view(), name='courses'),
     path('course/<int:pk>/', views.CourseView.as_view(), name='course'),
     path('user/<int:pk>/', views.UserView.as_view(), name='user'),
     path('enrollment/<int:pk>/', views.EnrollmentView.as_view(), name='enrollment'),
+    path('enrollment/', views.EnrollmentsView.as_view(), name='enrollments'),
 ]
--- graph/views.py
+++ graph/views.py
@@ -3,9 +3,23 @@ from django.views import generic
 
 from .models import Department, Course, User, Enrollment
 
+class DepartmentsView(generic.ListView):
+    template_name = 'graph/departments.html'
+
+    def get_queryset(self):
+        """Return all departments"""
+        return Department.objects.all()
+
 class DepartmentView(generic.DetailView):
     model = Department
     template_name = 'graph/department.html'
+
+class CoursesView(generic.ListView):
+    template_name = 'graph/courses.html'
+
+    def get_queryset(self):
+        """Return all courses"""
+        return Course.objects.all()
 
 class CourseView(generic.DetailView):
     model = Course
@@ -14,6 +28,13 @@ class CourseView(generic.DetailView):
 class UserView(generic.DetailView):
     model = User
     template_name = 'graph/user.html'
+
+class EnrollmentsView(generic.ListView):
+    template_name = 'graph/enrollments.html'
+     
+    def get_queryset(self):
+        """Return all enrollments"""
+        return Enrollment.objects.all()
 
 class EnrollmentView(generic.DetailView):
     model = Enrollment
